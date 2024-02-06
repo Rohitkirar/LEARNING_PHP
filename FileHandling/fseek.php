ca3e2910028b4738d8ce8376ade6858554966eb7
ROHITKIRAR <rohitkirar090@gmail.com>
2024-02-06 15:49:01 +0530

    completed function feof() , filesize(), fseek() , clearstatcache()

---
+++ FileHandling/fseek.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+fseek(fileresource , idx); it is used to change the pointer of file
+*/
+
+$myfile = fopen("OpenReadWriteFiles/demo1.txt" , "r") or die("Unable to open file") ;
+
+echo "before reading pointer : " . ftell($myfile) . "<br>";
+
+echo fread($myfile , filesize("OpenReadWriteFiles/demo1.txt"));
+
+echo "after reading pointer : " . ftell($myfile) . "<br>";
+
+fseek($myfile , 0 );
+
+echo "after fseek() used pointer : " . ftell($myfile) . "<br>";
+
+
+fclose($myfile);
+
+?>
