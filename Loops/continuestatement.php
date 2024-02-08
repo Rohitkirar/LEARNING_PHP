e9b89d23a5e6692101f070819aebc95c2f8c2518
Rohit kirar <108016847+Rohitkirar@users.noreply.github.com>
2024-02-08 18:56:54 +0530

    Delete Loops directory

--- Loops/continuestatement.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//The continue statement can be used to jump out of the current iteration of a loop, and continue with the next.
-for ($x = 0; $x < 10; $x++) {
-    if ($x == 4) {
-      continue;
-    }
-    echo "The number is: $x <br>\n";
-  }
-?>
