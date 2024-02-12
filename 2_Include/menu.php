e715b8078b61077594a71358500a6ff179527562
ROHITKIRAR <rohitkirar090@gmail.com>
2024-02-12 10:52:24 +0530

    change the name of file

--- 2_Include/menu.php
+++ 2_Include/menu.php
@@ -1,8 +1,18 @@
 <?php 
-echo '<a href="default.asp" > Home </a> - 
-        <a href="/html/default.asp">Html Tutorial</a> - 
-        <a href="/css/default.asp">Css Tutorial</a> -
-        <a href="/js/default.asp">Javascript tutorial </a> - 
-        <a href="default.asp"> PHP Tutorial</a>';
-        
+   
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+        <a href="default.asp" > Home </a>
+        <a href="/html/default.asp">Html Tutorial</a> 
+        <a href="/css/default.asp">Css Tutorial</a>
+        <a href="/js/default.asp">Javascript tutorial </a> 
+        <a href="default.asp"> PHP Tutorial</a>
+</body>
+</html>
