b284b73f42e308cd110f4261f400b947ea9b7ee0
ROHITKIRAR <rohit.silversky@gmail.com>
2024-02-16 18:22:41 +0530

    solve namespaces example

---
+++ 13_Namespaces/Example1.php
@@ -0,0 +1,12 @@
+<?php 
+namespace A ;
+class Main{
+    function print(){
+        echo "Printing Main Data From Namespace A <BR>" ;
+    }
+}
+
+function printa(){
+    echo "Printing Data outside function From Namespace A <BR>" ;
+}
+?>
---
+++ 13_Namespaces/Example2.php
@@ -0,0 +1,13 @@
+<?php 
+namespace B ; 
+class Main{
+    public function print(){
+        echo "printing Main Data from Namespace B<br>";
+    }
+}
+
+function printa(){
+    echo "printing Data outside function from Namespace B<br>";
+}
+
+?>
