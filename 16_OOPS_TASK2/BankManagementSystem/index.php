426599e58c3b49be2ac100ee6a9dde6db875f86d
ROHITKIRAR <rohit.silversky@gmail.com>
2024-02-16 19:29:47 +0530

    index file handle the operation of this class and function

---
+++ 16_OOPS_TASK2/BankManagementSystem/index.php
@@ -0,0 +1,32 @@
+<?php 
+require_once("Class/Bank.php");
+require_once("Class/User.php");
+
+// global array to store account details
+$accountDetails = []  ;
+
+$bank = new Bank();
+$bank->setNewUser(12345678, "hariom123" , "12345");
+$bank->setNewUser(1234414, "hariom123" , "12345");
+$bank->setNewUser(1354234515, "hariom123" , "12345");
+
+$user = new UserLogin(12345678 , "hariom123" , "12345");
+
+$user->checkBalance();
+$user->creditAmount(10000);
+$user->checkBalance();
+$user->withdrawAmount(5000);
+$user->checkBalance();
+
+echo "<pre>";
+print_r($accountDetails);
+
+print_r($bank->getUserDetails());
+
+$bank->saveRecords();
+
+$user->showhistory();
+
+echo "</pre>";
+
+?>
