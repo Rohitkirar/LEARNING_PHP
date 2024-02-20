c9f85c16a84b6345be917ebbc2facc3d6ea2e317
ROHITKIRAR <rohit.silversky@gmail.com>
2024-02-20 15:57:01 +0530

    see the operator precedence concept and solve example

--- MYSQL/6.2_OR.php
+++ MYSQL/6.2_OR.php
@@ -1,5 +1,12 @@
 <?php 
-//OR operator in where clause Example solved
+/**
+ * OR operator in where clause Example solved
+ * Summary
+ * The OR operator combines two Boolean expressions and returns true when either expression is true. Otherwise, it returns false.
+ * MySQL evaluates the OR operator after the AND operator if an expression contains both AND and OR operators.
+ * Use parentheses to change the order of evaluation.
+ */
+Echo "ExAMple 1 : <BR>";
 try{
     $conn = new PDO("mysql:host=localhost;dbname=classicmodels" , "root",  "");
     $query = "SELECT 
@@ -20,8 +27,64 @@ try{
     echo "<pre>";
     print_r($result);
     echo "</pre>";
+    $conn = null;
 }
 catch(PDOException $e){
     echo "ERROR : " . $e->getMessage();
 }
+
+ECHO "<hr>sExAMPLE 2 : <BR> ";
+{
+  $conn = new mysqli("localhost" , "root" , "" , "classicmodels");
+  $sql = "SELECT 
+            customername,
+            country
+          FROM
+            customers
+          WHERE 
+            country = 'USA' 
+          OR
+            country = 'France'
+            ;";
+  $queryResult = $conn->query($sql);
+  $resultArray = $queryResult->fetch_all(MYSQLI_ASSOC);
+  echo "<pre>" ; print_r($resultArray); echo "</pre>";
+}
+
+Echo "<HR>EXAMPLE 3 : <BR>";
+{
+  $sql = "SELECT
+              customername,
+              country,
+              creditLimit
+          FROM
+              customers
+          WHERE
+            (country = 'USA' OR country = 'France')
+          AND
+            creditLimit>100000;";
+  $queryResult = $conn->query($sql);
+  $resultArray = $queryResult->fetch_all(MYSQLI_ASSOC);
+  echo "<pre>" ; print_r($resultArray) ; echo "</pre>";
+}
+
+Echo "<HR>EXAMPLE 4 : <BR>";
+{
+  $sql = "SELECT
+              customername,
+              country,
+              creditLimit
+          FROM
+              customers
+          WHERE
+            country = 'USA' 
+          OR 
+            country = 'France'
+          AND
+            creditLimit>100000;";
+  $queryResult = $conn->query($sql);
+  $resultArray = $queryResult->fetch_all(MYSQLI_ASSOC);
+  echo "<pre>" ; print_r($resultArray) ; echo "</pre>";
+}
+
 ?>
