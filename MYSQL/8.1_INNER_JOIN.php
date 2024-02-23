0edb3638e8f83300a3bc3af13abda71a7d81cc1c
ROHITKIRAR <rohit.silversky@gmail.com>
2024-02-23 12:09:44 +0530

    solve example related to inner join by joining 3 and 4 four tables

--- MYSQL/8.1_INNER_JOIN.php
+++ MYSQL/8.1_INNER_JOIN.php
@@ -53,4 +53,102 @@ ORDER BY TOTALORDER DESC
 ";
 
 require("FILE/printdata.php");
+
+
+ECHO "<HR>EXAMPLE 4 :: Join 3 three table customers, orders, payments to find customer customernumber , customerName phone , fulladdress , salesRep , creditlimit , totalorder , totalamount<BR>";
+
+$sql = 
+"SELECT customerNumber , customerName , phone , CONCAT(addressLine1, ', ' , city, ', ' , state, ', ' , postalCode, ', ' , country) as Address , salesRepEmployeeNumber , creditLimit , count(orderNumber) as totalOrder , sum(amount) as totalAmount FROM customers 
+JOIN
+orders USING(customerNumber)
+JOIN
+payments
+USING(customerNumber)
+WHERE state IS NOT NULL 
+AND city IS NOT NULL
+AND postalCode IS NOT NULL
+AND country IS NOT NULL 
+AND addressLine1 IS NOT NULL
+GROUP BY customerNumber
+LIMIT 20;
+";
+
+require("FILE/printdata.php");
+
+
+ECHO "<HR>EXAMPLE 5 :: JOIN 3 tables orders , orderdetails and products and get orderNumber, orderdate , orderLineNumber , productName , quantityOrdered , priceEach<BR>";
+
+$sql = 
+"SELECT  o.orderNumber , o.orderdate , od.orderLineNumber , p.productName , od.quantityOrdered , od.priceEach
+FROM orders AS o 
+JOIN orderdetails AS od USING(orderNumber)
+JOIN products AS  p USING(productCode)
+ORDER BY o.orderNUMBER, 
+orderLineNumber
+LIMIT 20;
+";
+
+require("FILE/printdata.php");
+
+
+ECHO "<HR>EXAMPLE 6 :: get orderNumber , orderstatus and total sales from orders and orderdetails table using group by clause<BR>";
+
+$sql = 
+"SELECT o.orderNumber , o.status , sum(od.quantityOrdered * od.priceEach) as total
+FROM orders as o 
+JOIN 
+orderdetails as od
+ON o.orderNumber = od.orderNumber
+GROUP BY orderNumber
+LIMIT 20;
+";
+
+require("FILE/printdata.php");
+
+
+ECHO "<HR>EXAMPLE 7 :: get productCode , productname from product table and textDescription from productlines<BR>";
+
+$sql = 
+"SELECT p.productCode , p.productname , pl.textDescription 
+FROM products AS p 
+JOIN 
+productlines AS pl
+USING(productline)
+LIMIT 20;
+";
+
+require("FILE/printdata.php");
+
+
+ECHO "<HR>EXAMPLE : 8 : joining 4 tables customers, orders, orderdetails and products<BR>";
+
+$sql = 
+"SELECT orderNumber, orderDate , customerName , orderLineNumber , productName , quantityOrdered , priceEach
+FROM orders
+JOIN orderdetails USING (orderNumber)
+JOIN products USING(productCode)
+JOIN customers USING(customerNumber)
+WHERE orderNumber BETWEEN 10102 AND 10105
+ORDER BY orderNumber,
+orderLineNumber
+";
+
+require("FILE/printdata.php");
+
+
+ECHO "<HR>EXAMPLE  : 9 : In addition to the equal operator (=), you can use other operators such as greater than ( >), less than ( <), and not-equal ( <>) operator to form the join condition.
+<BR>";
+
+$sql = 
+"SELECT c.customerNumber , c.customerName , count(o.orderNumber) as totalOrder , sum(od.quantityOrdered*od.priceEach) as totalAmount
+FROM orders as o 
+JOIN orderdetails as od USING(orderNumber)
+JOIN customers as c ON o.customerNumber = c.customerNumber
+AND c.customerNumber BETWEEN 100 AND 200 
+GROUP BY customerNumber;
+";
+
+require("FILE/printdata.php");
+
+
 ?>
