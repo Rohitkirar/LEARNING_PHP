150e815138abb4ab055291ca238bce937d492f1a
ROHITKIRAR <rohit.silversky@gmail.com>
2024-02-27 14:30:10 +0530

    RENAME TABLE by RENAME and ALTER SQL Statement

--- MYSQL/FILE/printdata.php
+++ MYSQL/FILE/printdata.php
@@ -1,22 +1,40 @@
 <?php 
 $queryResult = $conn->query($sql);
-$result = $queryResult->fetch_all(MYSQLI_ASSOC);
-echo "<table style='border:1px solid black; text-align:center; padding:12px;'>";
 
-foreach($result as $values){
-    echo "<tr>";
-    foreach($values as $key => $value ){
-        echo "<th  style='text-align:center;'>$key</th>";
+if($queryResult){
+
+    if($queryResult === true){
+        echo "<HR>QUERY EXCUTED SUCCESSFULLY :  ";
+        print_r($queryResult) ;
+        echo "<BR>";
     }
-    echo "</tr>";
-    break;
-}
-foreach($result as $values){
-    echo "<tr>";
-    foreach($values as $value ){
-        echo "<td>$value</td>";
+    else{
+        $result = $queryResult->fetch_all(MYSQLI_ASSOC);
+
+        echo "<table style='border:1px solid black; text-align:center; padding:12px;'>";
+
+        foreach($result as $values){
+            echo "<tr>";
+            foreach($values as $key => $value ){
+                echo "<th  style='text-align:center;'>$key</th>";
+            }
+            echo "</tr>";
+            break;
+        }
+
+        foreach($result as $values){
+            echo "<tr>";
+            foreach($values as $value ){
+                echo "<td>$value</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
     }
-    echo "</tr>";
 }
-echo "</table>";
+else{
+    echo "<HR>ERROR : ". mysqli_error($conn) ."<BR>";
+}
+
+
 ?>
