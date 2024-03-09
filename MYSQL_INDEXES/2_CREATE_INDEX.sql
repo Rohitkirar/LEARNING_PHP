613efda78c36e8589498348537ddf25737565542
ROHITKIRAR <rohit.silversky@gmail.com>
2024-03-09 12:55:26 +0530

    add more example of index

--- MYSQL_INDEXES/2_CREATE_INDEX.sql
+++ MYSQL_INDEXES/2_CREATE_INDEX.sql
@@ -6,6 +6,33 @@ Use the CREATE INDEX statement to create a new index for a table.
 
 -- the following statement creates a new table with an index that consists of two columns c2 and c3.
 USE classicmodels;
+
+-- without creating index for city
+EXPLAIN SELECT * FROM customers where city = 'NYC'; -- search 98 result 5 row only
+
+-- creating index for city
+
+CREATE INDEX city ON customers(city);
+
+EXPLAIN SELECT * FROM customers WHERE city = 'NYC'; -- search 5 rows result 5row 
+
+
+-- without creating index for country
+EXPLAIN SELECT * FROM customers where country = 'USA'; -- search 98 result 35 row only
+
+-- creating index for country
+CREATE INDEX country ON customers(country);
+
+EXPLAIN SELECT * FROM customers WHERE  country = 'USA'; -- search 35 rows result 35 row row 
+
+SHOW INDEXES FROM customers;
+
+DROP INDEX city ON customers;
+DROP INDEX country ON customers;
+
+-- multiple col index creation
+CREATE INDEX city_country ON customers(city , country); -- doubt
+
 CREATE TABLE t(
     c1 int primary key , 
     c2 int not null,
