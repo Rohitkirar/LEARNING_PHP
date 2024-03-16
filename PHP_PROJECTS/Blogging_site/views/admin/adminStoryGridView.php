6c4e45b62e918fcb3bd4f808ceab36cabfb0c44e
ROHITKIRAR <rohit.silversky@gmail.com>
2024-03-16 19:09:00 +0530

    add css the files

--- PHP_PROJECTS/Blogging_site/views/admin/adminStoryGridView.php
+++ PHP_PROJECTS/Blogging_site/views/admin/adminStoryGridView.php
@@ -1,4 +1,11 @@
+<style>
+    .grid-item{
+        background-image: url('../../uploads/7042259.jpg');
+        
+    }
+</style>
 <?php
+
  
     $sql = "SELECT story.id as story_id , category.Title as category_title , story.title as story_title
     FROM category JOIN story 
@@ -10,27 +17,27 @@
 
     $storyArray = mysqli_fetch_all($result , MYSQLI_ASSOC);
 
+    echo "<div style=' display:grid; grid-template-columns:auto auto auto;' >";
 
     foreach($storyArray as $key=>$values){
             echo "
             <div class='grid-item' >
-
+                
                 <h6 style='color:purple'>Title : {$values['story_title']} </h3><BR>
 
                 <h6 style='color:purple'>Category : {$values['category_title']} </h3><BR>
+                
                 <div class='btn-group'>
-                <button class='btn btn-primary'>
-                    <a href='adminStoryView.php?story_id={$values['story_id']}' style='text-decoration:none;color:white;'>View</a>
-                </button>
 
-                <button id='updatebtn' class='btn btn-secondary'>
-                    <a href='updateStoryForm.php?story_id={$values['story_id']}' style='text-decoration:none;color:white;'>Update Story</a>
-                </button>
+                    <a href='adminStoryView.php?story_id={$values['story_id']}' class='btn btn-primary'>View</a>
 
-                <button id='deletebtn' class='btn btn-danger'>
-                    <a href='deleteStory.php?story_id={$values['story_id']}' style='text-decoration:none; color:white;'>Delete Story</a>
-                </button>
+                    <a href='updateStoryForm.php?story_id={$values['story_id']}' class='btn btn-secondary' >Update Story</a>
+
+                    <a href='deleteStory.php?story_id={$values['story_id']}' class='btn btn-danger'>Delete Story</a>
+                
                 </div>
+
             </div>";
     }
+    echo "</div>"
 ?>    
