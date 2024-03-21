ef4ae2e33157993c3b9d272cd8820ae7e1d528bb
ROHITKIRAR <rohit.silversky@gmail.com>
2024-03-21 12:19:46 +0530

    format home page login page register page

--- PHP_PROJECTS/Blogging_site/views/common/Register.php
+++ PHP_PROJECTS/Blogging_site/views/common/Register.php
@@ -133,9 +133,9 @@ if(isset($_POST['submit'])){
 <head>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../../public/css/register1.css">
-    <link rel="stylesheet" href="../../public/css/home1.css">
-    <link rel="stylesheet" href="../../public/css/style1.css">
+    <link rel="stylesheet" href="../../public/css/userform.css">
+    <link rel="stylesheet" href="../../public/css/csshome.css">
+    <!-- <link rel="stylesheet" href="../../public/css/style1.css"> -->
     <!-- <link rel="stylesheet" href="../../public/css/style.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    
 </head>
--- PHP_PROJECTS/Blogging_site/views/common/logout.php
+++ PHP_PROJECTS/Blogging_site/views/common/logout.php
@@ -11,16 +11,18 @@ session_destroy();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Homepage</title>
     <link rel="stylesheet" href="../../public/css/csshome.css">
-    <link rel="stylesheet" href="../../public/css/style1.css">
+    <!-- <link rel="stylesheet" href="../../public/css/style1.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">   
 </head>
 <body>
     <!-- adding navbar file -->
     <?php require_once('navbar.php') ?>
 
-    <main class="text-center">
+    <main class="text-center" style="color:black;">
+        <br>
         <h1>Logout Successfully</h1>
-        <a href="Login.php" rel="noopener noreferrer" style="text-decoration: none; color:white">Login Again</a>
+
+        <a href="Login.php" rel="noopener noreferrer" style="text-decoration: none;">Login Again</a>
         <!-- <a href="Register.php"  rel="noopener noreferrer">REGISTER</a> -->
         <p></p>
     </main>
