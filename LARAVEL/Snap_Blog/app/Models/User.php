ecbca3b0736a2a44f92142072c1e7689a6c3c6b6
ROHITKIRAR <rohit.silversky@gmail.com>
2024-04-08 18:26:34 +0530

    create model class for PostCategory , Post , PostImage , PostLike , PostComment

--- LARAVEL/Snap_Blog/app/Models/User.php
+++ LARAVEL/Snap_Blog/app/Models/User.php
@@ -18,9 +18,14 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'gender',
+        'date_of_birth',
         'email',
-        'password',
+        'number',
+        'username',
+        'password'
     ];
 
     /**
